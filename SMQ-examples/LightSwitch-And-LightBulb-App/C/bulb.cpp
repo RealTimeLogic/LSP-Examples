fb94bd1c97b3cd8f7ce0d0291411faa7c97dd6db
RTL-pub <ginfo@realtimelogic.com>
2022-01-11 16:21:32 -0800

    Adding C++ example

--- SMQ-examples/LightSwitch-And-LightBulb-App/C/bulb.cpp
+++ SMQ-examples/LightSwitch-And-LightBulb-App/C/bulb.cpp
@@ -1,10 +1,10 @@
 /*
-  The C Code Companion Example for the article:
+  The C++ Code Companion Example for the article:
   https://realtimelogic.com/articles/Modern-Approach-to-Embedding-a-Web-Server-in-a-Device
   GitHub:
   https://github.com/RealTimeLogic/LSP-Examples/tree/master/SMQ-examples/LightSwitch-And-LightBulb-App
 
-  See also the C++ version bulb.cpp
+  See also the C version bulb.c
 
   This example uses the SMQ protocol: https://realtimelogic.com/ba/doc/en/SMQ.html
 
@@ -83,31 +83,28 @@ static int checkBufOverflow(BufPrint* bp, int sizeRequired)
    JEncoder: https://realtimelogic.com/ba/doc/en/C/reference/html/structJEncoder.html
    SMQ: https://realtimelogic.com/ba/doc/en/C/reference/html/structSMQ.html
  */
-static void pubSwitchOnOff(SMQ* smq, BaBool bulbOn, U32 tid, U32 subtid)
+static void pubSwitchOnOff(SMQ& smq, BaBool bulbOn, U32 tid, U32 subtid)
 {
-   JErr err;
-   JEncoder encoder;
-   BufPrint bp;
    char buf[15];
-   JErr_constructor(&err);
-   BufPrint_constructor(&bp, 0, checkBufOverflow);
+   JErr err;
+   BufPrint bp(NULL, checkBufOverflow);
    BufPrint_setBuf(&bp, buf, sizeof(buf));
-   JEncoder_constructor(&encoder, &err, &bp);
-   JEncoder_set(&encoder, "{b}", "on", bulbOn);
-   JEncoder_flush(&encoder); /* Function checkBufOverflow gets called */
-   SMQ_publish(smq,bp.buf,bp.cursor,tid,subtid);
+   JEncoder encoder(&err, &bp);
+   encoder.set("{b}", "on", bulbOn);
+   encoder.flush(); /* Function checkBufOverflow gets called */
+   smq.publish(bp.buf,bp.cursor,tid,subtid);
 }
 
 #else /* Not: USE_JSON */
 
 /* Publish manually crafted JSON message for turning switch on or off.
  */
-static void pubSwitchOnOff(SMQ* smq, BaBool bulbOn, U32 tid, U32 subtid)
+static void pubSwitchOnOff(SMQ& smq, BaBool bulbOn, U32 tid, U32 subtid)
 {
    static const char* onJson="{\"on\":true}";
    static const char* offJson="{\"on\":false}";
    const char* json = bulbOn ? onJson : offJson;
-   SMQ_publish(smq, json,strlen(json),tid,subtid);
+   smq.publish(json,strlen(json),tid,subtid);
 }
 
 #endif /* USE_JSON */
@@ -123,33 +120,29 @@ static void pubSwitchOnOff(SMQ* smq, BaBool bulbOn, U32 tid, U32 subtid)
  */
 static void bulb()
 {
-   SMQ smq;
    U8 smqBuf[256];
+   SMQ smq(smqBuf,sizeof(smqBuf));
    U32 helloTid,setTid,setSubTid; /* Topic and sub topic IDs used in this example */
    U8* msg;
    BaBool bulbOn=FALSE;
 
 #if USE_JSON
    char maxMembN[4]; /* Buffer for holding temporary JSON member name */
-   JParserValFact pv; /* JSON Parser Value (JVal) Factory */
-   JParser parser; /* JSON parser */
+   /* JSON Parser Value (JVal) Factory */
+   JParserValFact pv(AllocatorIntf::getDefault(), AllocatorIntf::getDefault());
+   /* JSON parser */
+   JParser parser(&pv, maxMembN, sizeof(maxMembN), AllocatorIntf_getDefault(),0);
    JErr jerr;
-   JParserValFact_constructor(&pv, AllocatorIntf_getDefault(),
-                              AllocatorIntf_getDefault());
-   JParser_constructor(&parser, (JParserIntf*)&pv, maxMembN,
-                       sizeof(maxMembN), AllocatorIntf_getDefault(),0);
-   JErr_constructor(&jerr);
 #endif
 
-   SMQ_constructor(&smq,smqBuf,sizeof(smqBuf));
-   if(SMQ_init(&smq, SMQ_URL, 0) < 0)
+   if(smq.init(SMQ_URL, 0) < 0)
    {  /* Error codes:
          https://realtimelogic.com/ba/doc/en/C/reference/html/group__SMQClientErrorCodes.html#SMQClientErrorCodes
       */
       printf("Cannot establish connection, status: %d\n", smq.status);
       return;
    }
-   if(SMQ_connect(&smq, getUniqueID(), strlen(getUniqueID()),
+   if(smq.connect(getUniqueID(), strlen(getUniqueID()),
                   0, 0, /* credentials */
                   0, 0 /* info */))
    {
@@ -163,25 +156,25 @@ static void bulb()
       See Topic and sub topic names for details:
       https://realtimelogic.com/ba/doc/?url=SMQ.html#TopicNames
     */
-   SMQ_subscribe(&smq, "/switchui/hello");
-   SMQ_create(&smq, "/switch/set");
-   SMQ_createsub(&smq, "/switch/set");
+   smq.subscribe( "/switchui/hello");
+   smq.create( "/switch/set");
+   smq.createsub( "/switch/set");
    /* Simplified error management below: */
-   if(SMQ_SUBACK != SMQ_getMessage(&smq, &msg)) return; /* failed */
+   if(SMQ_SUBACK != smq.getMessage(&msg)) return; /* failed */
    helloTid=smq.ptid; /* Topic "/switchui/hello" */
-   if(SMQ_CREATEACK != SMQ_getMessage(&smq, &msg)) return; /* failed */
+   if(SMQ_CREATEACK != smq.getMessage(&msg)) return; /* failed */
    setTid=smq.ptid; /* Topic "/switch/set" */
-   if(SMQ_CREATESUBACK != SMQ_getMessage(&smq, &msg)) return; /* failed */
+   if(SMQ_CREATESUBACK != smq.getMessage(&msg)) return; /* failed */
    setSubTid=smq.ptid; /* Sub-topic "/switch/set" */
    /* Inform all connected switch viewers (switch.html) that we just
     * started; publish One-2-many.
     */
-   pubSwitchOnOff(&smq,bulbOn,setTid,0);
+   pubSwitchOnOff(smq,bulbOn,setTid,0);
    for(;;)
    {
       U8* msg;
       /* Wait for the next message */
-      int len =  SMQ_getMessage(&smq, &msg);
+      int len =  smq.getMessage(&msg);
       if(len < 0) /* We received a control message or an error code */
       {
          if(len == SMQE_TIMEOUT)
@@ -195,7 +188,7 @@ static void bulb()
       {
          printf("A new switch with tid %u connected\n", smq.ptid);
          /* Send state back to sender; publish one-2-one */
-         pubSwitchOnOff(&smq,bulbOn,smq.ptid,setSubTid);
+         pubSwitchOnOff(smq,bulbOn,smq.ptid,setSubTid);
       }
       /* If topic == "self" (one-to-one communication). An SMQ client
        * is auto subscribed to topic "self". Compare this code with
@@ -206,9 +199,9 @@ static void bulb()
          /* Our topic design uses only one sub-topic: "/switch/set" */
          assert(smq.subtid == setSubTid);
 #if USE_JSON
-         if(JParser_parse(&parser, msg, len) <= 0 ||
-            (JVal_get(JParserValFact_getFirstVal(&pv), &jerr, "{b}", "on", &bulbOn),
-             JErr_isError(&jerr)))
+         if(parser.parse(msg, len) <= 0 ||
+            (pv.getFirstVal()->get(&jerr, "{b}", "on", &bulbOn),
+             jerr.isError()))
          {
             printf("JSON error\n");
             return;
@@ -219,7 +212,7 @@ static void bulb()
 #endif
          printf("Switch with tid %u set bulb %s\n", smq.ptid, bulbOn ? "on" : "off");
          /* Inform all connected switch viewers (switch.html); publish one-2-many. */
-         pubSwitchOnOff(&smq,bulbOn,setTid,0);
+         pubSwitchOnOff(smq,bulbOn,setTid,0);
       }
       else
          return; /* Simplified  error management;  Unknown SMQ topic (tid) */
