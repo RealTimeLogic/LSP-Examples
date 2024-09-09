deab853a8b6efbe3c151c43d4bc6dbde13109a2a
RTL <RTL-pub@users.noreply.github.com>
2024-09-09 16:40:46 -0700

    Authentication now using TPM + new add users page

--- Light-Dashboard/www/.lua/cms.lua
+++ Light-Dashboard/www/.lua/cms.lua
@@ -1,21 +1,29 @@
 
 -- Mini Content Management System (CMS) designed for the Pure.css dashboard.
 
+-- All LSP applications have a predefined 'dir' object, which is a resrdr instance.
+-- Ref resrdr: https://realtimelogic.com/ba/doc/en/lua/lua.html#ba_create_resrdr
+-- The dir object is not set when running as an Xedge xlua app (not LSP enabled app)
+-- https://realtimelogic.com/ba/doc/en/Xedge.html#using
+if not dir then
+   error"This application must run as an LSP enabled app"
+end
+
 local fmt=string.format
 
 -- Load module for reading and writing raw-files and json-files.
--- Details: https://realtimelogic.com/ba/doc/?url=Mako.html#rwfile
+-- Details: https://realtimelogic.com/ba/doc/en/lua/auxlua.html#rwfile
 local rw=require"rwfile"
 
 -- Convert to closures so variables work in the cmsfunc() callback function
 -- Closure def: https://en.wikipedia.org/wiki/Closure_(computer_programming)
-local app,io,dir=app,app.io,dir
+local app,io,dir=app,app.io,app.dir
 
 
 -- Takes the data content of an LSP page (HTML with LSP tags) as
 -- argument, converts the LSP page to Lua code, and compiles the Lua
 -- code. The compiled Lua code is returned as a function.
--- Details: https://realtimelogic.com/ba/doc/?url=lua.html#ba_parselsp
+-- Details: https://realtimelogic.com/ba/doc/en/lua/lua.html#ba_parselsp
 local function parseLspPage(name)
    local func
    local data,err=rw.file(io,name) -- Read file content
@@ -52,14 +60,14 @@ end
 -- An LSP page expects a persistent table unique to each page. The
 -- pagesT is a table where the key is the relative path and the value
 -- is the LSP page's unique table.
--- Details: https://realtimelogic.com/ba/doc/?url=lua.html#CMDE
+-- Details: https://realtimelogic.com/ba/doc/en/lua/lua.html#CMDE
 local pagesT={}
 
 -- The directory callback function. This callback is called by the
 -- server when a resource is accessed. Argument env is the
 -- request/response environment table and relpath is the relative path.
 -- See the following for details on the request/response environment:
--- https://realtimelogic.com/ba/doc/?url=lua.html#CMDE
+-- https://realtimelogic.com/ba/doc/en/lua/lua.html#CMDE
 local function cmsfunc(_ENV, relpath, notInMenuOK)
 
    -- Translate to (path/)index.html if only directory name is provided.
@@ -88,14 +96,14 @@ local function cmsfunc(_ENV, relpath, notInMenuOK)
    -- Make the following available to template.lsp
    -- Note, we explicitly use the _ENV tab for code readability.
    -- Details: https://realtimelogic.com/ba/doc/en/lua/man/manual.html#2.2
-   --          https://realtimelogic.com/ba/doc/?url=lua.html#CMDE
+   --          https://realtimelogic.com/ba/doc/en/lua/lua.html#CMDE
    _ENV.menuL=menuL
    _ENV.menuT=menuT
    _ENV.relpath=relpath
    -- lspPage is the parsed page (function as explained in parseLspPage above)
    _ENV.lspPage=parseLspPage(".lua/www/"..relpath)
    -- Call the template page and pass in the required arguments.
-   -- Arg details: https://realtimelogic.com/ba/doc/?url=lua.html#ba_parselsp
+   -- Arg details: https://realtimelogic.com/ba/doc/en/lua/lua.html#ba_parselsp
    templatePage(_ENV,relpath,io,pageT,app)
    -- non cached version of above. Use if testing new template.
    --parseLspPage(".lua/www/template.lsp")(_ENV,relpath,io,pageT,app)
@@ -108,84 +116,80 @@ end
 -- construction makes sure we do not trigger the callback for any
 -- static assets. In other words, static assets take precedence. The
 -- callback is called when no static asset is found.
--- Ref dir: https://realtimelogic.com/ba/doc/?url=lua.html#ba_create_dir
+-- Ref dir: https://realtimelogic.com/ba/doc/en/lua/lua.html#ba_create_dir
 local cmsDir = ba.create.dir()
 cmsDir:setfunc(cmsfunc)
 
 
-------------------- AUTHENTICATION ---------------------------------
--- All code below is for setting up authentication and authorization
-
--- Default realm used by the server (can be changed).
-local realm = "Barracuda Server"
-
--- HA1 = MD5(username ":" realm ":" password)
--- https://realtimelogic.com/ba/doc/en/lua/auth.html
-local function ha1(realm,username,password)
-   return ba.crypto.hash"md5"(username)":"(realm)":"(password)(true,"hex")
-end
-
-
--- Hard coded user database, with the following users: admin,user
--- All users have the same password: qwerty
-local userdb = {
-   user={ pwd={ha1(realm, "user", "qwerty")}, roles={"user"} },
-   admin={ pwd={ha1(realm, "admin", "qwerty")}, roles={"admin"} },
-}
-
--- Hard coded constraints
-local constraints = {
-   user={
-      urls={'/*'},
-      methods={'GET'},
-      roles={'user'}
-   },
-   admin={
-      urls={'/*'},
-      methods={'GET','POST'},
-      roles={'admin'}
-   },
-}
-
--- JSON User Database: https://realtimelogic.com/ba/doc/?url=lua.html#ba_create_jsonuser
-local ju=ba.create.jsonuser()
-assert(ju:set(userdb)) -- Insert JSON string or Lua table (We use Lua table)
-local ja=ju:authorizer()
-assert(ja:set(constraints)) -- Insert JSON string or Lua table (We use Lua table)
-
--- ba.create.authenticator() callback function
-local function loginresponse(_ENV, authinfo)
-   -- How _ENV is used: https://realtimelogic.com/ba/doc/?url=lua.html#CMDE
-   _ENV.authinfo = authinfo -- Makes it possible for .login-form.lsp to use authinfo
-   authinfo.realm=realm -- Used by login.lsp
-   -- The following prints the content of the authinfo table
-   trace("loginresponse", ba.json.encode(authinfo))
-   cmsfunc(_ENV,"login.html", true) -- Let the CMS function emit the login page.
-end
-
--- Create the authenticator that will be applied to the 'pages' directory.
-local authenticator=ba.create.authenticator(ju,{response=loginresponse, type="form", realm=realm})
-local authDir = ba.create.dir(1) -- No name matches "", set priority to a value greater than cmsDir.
-authDir:setauth(authenticator,ja)
-authDir:p403("/.lua/www/no-access.lsp")
-
--- All LSP applications have a predefined 'dir' object, which is a resrdr instance.
--- Ref resrdr: https://realtimelogic.com/ba/doc/?url=lua.html#ba_create_resrdr
--- The dir object is not set when running as an Xedge xlua app (not LSP enabled app)
--- https://realtimelogic.com/ba/doc/en/Xedge.html#using
-if not dir then
-   -- The resource reader serves the static content and is executed
-   -- before the authenticator triggers.
-   dir=ba.create.resrdr(io) -- no name, thus root.
-   dir:lspfilter() -- LSP enable. We have one LSP file accessed via authDir:p403()
-   dir:insert()
-end
-
--- Insert authDir and cmsDir as 'dir' siblings.
-dir:insert(authDir,true)
+-- Insert cmsDir as 'dir' siblings. This means the parent will be
+-- searched first. The parent resource reader (dir) manages the static
+-- content. See www/static for content returned to browser
 dir:insert(cmsDir,true)
 
--- Return onunload handler
-return function()
-   dir:unlink() -- Required when run as an Xedge xlua app.
+
+
+----------------------- AUTHENTICATION -----------------------------
+
+if ba.tpm then
+   -- The following code is based on example from:
+   -- https://realtimelogic.com/ba/doc/en/lua/auxlua.html#TPM
+
+   local cfgio = ba.openio"home" or ba.openio"disk" -- mako or xedge
+   local rw=require"rwfile"
+
+   -- Read/write encrypted db. Write if 'encdb' provided
+   local function rwdb(encdb)
+      trace(encdb and "Writing" or "Reading","userdb.encrypted")
+      return rw.file(cfgio,"userdb.encrypted",encdb)
+   end
+
+   -- ba.create.authenticator() callback function
+   local function loginresponse(_ENV, authinfo)
+      -- How _ENV is used: https://realtimelogic.com/ba/doc/en/lua/lua.html#CMDE
+      _ENV.authinfo = authinfo -- Makes it possible for .login-form.lsp to use authinfo
+      -- The following prints the content of the authinfo table
+      trace("loginresponse", ba.json.encode(authinfo))
+      cmsfunc(_ENV,"login.html", true) -- Let the CMS function emit the login page.
+   end
+
+   -- Create the wrapper and make the encrypted DB global
+   local tju=ba.tpm.jsonuser("dashboard",true)
+
+   local authDir=nil
+   local function setOrRemoveAuth()
+      if #tju.users() > 0 and not authDir then -- SET
+         authDir = ba.create.dir(1) -- Set priority to a value greater than cmsDir.
+         local authenticator=ba.create.authenticator(tju.getauth(),{response=loginresponse, type="form"})
+         authDir:setauth(authenticator)
+         dir:insert(authDir,true) -- Higher prio than cmsDir thus executes before 
+         trace"Installing authenticator"
+      elseif #tju.users() == 0 and authDir then -- REMOVE
+         authDir:unlink() -- Remove authenticator
+         authDir=nil
+         trace"Removing authenticator"
+      end
+   end
+
+   function setuser(name,pwd) -- Used by www/.lua/www/Users.html 
+      rwdb(tju.setuser(name,pwd))
+      setOrRemoveAuth()
+   end
+ 
+   local encdb=rwdb() -- Load the encryted DB, if any
+   if encdb then
+      -- Set the DB
+      local ok,err=tju.setdb(encdb)
+      if ok then
+         setOrRemoveAuth()
+      else
+         trace("Authenticator not installed; User DB error:",err)
+      end
+   else
+      trace"No user database; Authenticator not installed"
+   end
+else
+   trace"No TPM. Authenticator not installed"
 end
+
+-- Return onunload handler
+return function() end -- Not used
